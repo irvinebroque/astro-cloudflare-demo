dec75402ebc94ea3b8d8f509d9c6b91e00243b78
Brendan Irvine-Broque <brendanib@gmail.com>
2023-12-26 22:29:26 -0800

    rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
