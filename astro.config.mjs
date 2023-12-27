c87408f907abaf3ef473cd6628fac38c24de74bc
Brendan Irvine-Broque <brendanib@gmail.com>
2023-12-26 22:29:08 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
 
-import deno from "@astrojs/deno";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: deno()
+  adapter: cloudflare()
 });
